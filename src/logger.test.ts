a1d7c380dc727813eb48ada56e99f9a73193a090
claramarsango <claraxmarsango@gmail.com>
2023-03-08 21:26:27 +0100

    delete uneccessary logger test

--- src/logger.test.ts
+++ src/logger.test.ts
@@ -5,10 +5,4 @@ describe('Given a log function', () => {
     const log = (await import('./logger')).default;
     expect(log).toBeInstanceOf(bunyan);
   });
-
-  test('When the log level environment variable is not defined, then the log level should be info', async () => {
-    process.env.BUNYAN_LEVEL = undefined;
-    const log = (await import('./logger')).default;
-    expect(log.level()).toBe(20);
-  });
 });
